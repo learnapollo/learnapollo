8ef1d95a0f3474b2db4ce6d4655179b2cabb4499
Johannes Schickling <schickling.j@gmail.com>
2017-01-09 18:52:32 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -58,7 +58,7 @@ module.exports = {
       __GA_TRACKING_CODE__: '"UA-74131346-6"',
       __GITHUB_OAUTH_CLIENT_ID__: '"f83113f8d6c4d78f17e5"',
       __LAMBDA_AUTH__: '"https://73zvpdo0k5.execute-api.eu-west-1.amazonaws.com/prod/lambda-learnapollo-dev_auth"',
-      __LAMBDA_DOWNLOAD_EXAMPLE__: '"https://73zvpdo0k5.execute-api.eu-west-1.amazonaws.com/prod/lambda-file-processing_getting-started-example"',
+      __LAMBDA_DOWNLOAD_EXAMPLE__: '"https://qtkqo6so2i.execute-api.eu-west-1.amazonaws.com/Prod"',
     }),
     new HtmlWebpackPlugin({
       favicon: 'static/favicon.png',
